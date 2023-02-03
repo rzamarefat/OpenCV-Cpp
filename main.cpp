cc54e0c3c023f4ebf9acb91c8104ae059b4008c4
rzamarefat <rezamarefat@gmail.com>
2023-02-03 14:13:21 +0330

    Progress

--- main.cpp
+++ main.cpp
@@ -14,21 +14,77 @@ using namespace cv;
 	
 // 	imshow("Image", img);
 // 	waitKey(0);
+
+//  return 1;
 // }
 
-// ==========================> reading images
+// ==========================> reading video
+// int main(){
+// 	string path_to_video = "./resources/film.mp4";
+// 	VideoCapture cap(path_to_video);
+// 	Mat img;
+
+// 	while(true){
+// 		cap.read(img);
+// 		imshow("Video", img);
+// 		waitKey(1);	
+// 	}
+
+// 	return 1;
+// }
+
+
+
+// ==========================> webcam
+// int main(){
+	
+// 	VideoCapture cap(0);
+// 	Mat img;
+
+// 	while(true){
+// 		cap.read(img);
+// 		imshow("Video", img);
+// 		waitKey(1);	
+// 	}
+
+// 	return 1;
+// }
+
+
+// ==========================> image pre-processing
 int main(){
-	string path_to_video = "resources/film.mp4";
-	VideoCapture cap(path_to_video);
-	Mat img;
+	string path = "/home/rzamarefat/cpp-opencv/resources/brad.jpeg";
 
-	while(true){
-		img = cap.read(img);
+	Mat img = imread(path);
+	
 
-		imshow("Video", img);
+	// ===> Grayscale
+	// Mat img_gray;
+	// cvtColor(img, img_gray, COLOR_BGR2GRAY);
+	// imshow("Image Gray", img_gray);
 
-		waitKey(1);	
-	}
 
-	return 1;
+
+	// ===> Blurring
+	// Mat blurred_img;
+	// GaussianBlur(img, blurred_img, Size(7, 7), 5, 0);
+	// imshow("Blurred Image", blurred_img);
+
+
+	// ===> Edge Detection
+	Mat blurred_img;
+	Mat canny_img;
+	GaussianBlur(img, blurred_img, Size(7, 7), 5, 0);
+	Canny(blurred_img, canny_img, 50, 150);
+	imshow("Canny Image", canny_img);
+
+	
+
+
+	
+	
+
+	waitKey(0);
+
+	return 0;
 }
